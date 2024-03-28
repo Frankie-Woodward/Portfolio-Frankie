38c5409900114d124b591e235bbc5387dfc73b57
Frankie <142553084+Frankie-Woodward@users.noreply.github.com>
2024-03-28 16:07:32 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-Frankie/"
+  base: '/Portfolio-Frankie/',  // Adjust if necessary
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      // Rollup options if needed
+    }
+  }
 });
