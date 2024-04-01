a9c407e8b690953b7136b14e54b130daf96324e0
Frankie <fwoodwar@gmail.com>
2024-04-01 15:21:31 -0600

    fixing https

--- src/utils/backend.js
+++ src/utils/backend.js
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 const apiBaseUrl = 'https://localhost:3000/api/messages';
 
+const axiosInstance = axios.create({
+    baseURL: apiBaseUrl,
+    timeout: 5000, // Adjust timeout as needed
+    // Other Axios configurations
+});
 // Create a new message
 export async function createMessage(data) {
     try {
@@ -16,18 +21,8 @@ export async function createMessage(data) {
 // Get all messages
 export async function getMessages() {
     try {
-        const response = await axios.get(apiBaseUrl); // Use apiBaseUrl directly without appending '/'
-        const responseData = response.data;
-   
-
-        console.log('Response Data:', responseData); // Log the response data
-
-        if (Array.isArray(responseData) && responseData.length > 0) {
-            return responseData;
-        } else {
-            console.warn('Empty response or invalid data format');
-            return []; // Return an empty array or handle empty response as needed
-        }
+        const response = await axiosInstance.get('/');
+        return response.data;
     } catch (error) {
         console.error('Error getting messages:', error);
         throw error;
